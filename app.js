a313ddf5c0d45daf672dded7ac61e0963d81736c
Nico Nap <nico.nap@outlook.com>
2021-07-20 20:31:35 +0200

    Add membersonly route

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var dotenv = require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var membersonlyRouter = require('./routes/membersonly');
 
 var app = express();
 
@@ -27,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/membersonly', membersonlyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
