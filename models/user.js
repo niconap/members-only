a707d47e123c4ece60c46046c20525958c85bc7b
Nico Nap <nico.nap@outlook.com>
2021-07-27 07:44:00 +0200

    Change field to Boolean

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ var UserSchema = new Schema({
   last_name: { type: String, minLength: 3, maxLength: 100, required: true },
   username: { type: String, minLength: 3, maxLength: 100, required: true },
   password: { type: String, minLength: 8, required: true },
-  membership_status: { type: String, required: true }
+  membership_status: { type: Boolean, required: true }
 })
 
 UserSchema.virtual('url').get(function() {
