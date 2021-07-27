77c11ad206ce3f211c132e8160138daba389ddfe
Nico Nap <nico.nap@outlook.com>
2021-07-27 07:43:39 +0200

    Add ability to join the club

---
+++ controllers/joinController.js
@@ -0,0 +1,41 @@
+const User = require('../models/user');
+const Message = require('../models/message')
+const { body, validationResult} = require('express-validator');
+const async = require('async');
+
+// Render the join form
+exports.join_get = function(req, res, next) {
+  res.render('join_form', { title: "Join the club" })
+}
+
+exports.join_post = [
+  body('user_name', 'Username must be longer than 3 characters.').trim().isLength({ min: 3 }).escape(),
+  body('user_name', 'Username must not be longer than 100 characters.').trim().isLength({ max: 100 }).escape(),
+  // Check if username is in the database, if not: reject
+  body('user_name', 'Username is already in use.').custom((value, { req }) => {
+    return new Promise((resolve, reject) => {
+      User.findOne({ username: req.body.user_name }, function(err, user) {
+        if (err) return next(err);
+        if(user) {
+          resolve(true);
+        }
+        reject(new Error('Username is not in use.'))
+      });
+    });
+  }),
+  // Check if the entered passcode is correct
+  body('passcode', 'Passcode is incorrect.').equals(process.env.PASSCODE),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render('join_form', { title: "Sign up", errors: errors.array() });
+    } else {
+      User.findOneAndUpdate({ username: req.body.user_name }, { membership_status: true }, { new: true }, function(err, theuser) {
+        if (err) return next(err);
+        res.redirect('/');
+      })
+    }
+  }
+]
--- routes/membersonly.js
+++ routes/membersonly.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var signupController = require('../controllers/signupController');
+var joinController = require('../controllers/joinController');
 
 router.get('/', function(req, res, next) {
   res.send('NOT IMPLEMENTED')
@@ -9,5 +10,9 @@ router.get('/', function(req, res, next) {
 router.get('/signup', signupController.signup_get)
 
 router.post('/signup', signupController.signup_post);
+
+router.get('/join', joinController.join_get);
+
+router.post('/join', joinController.join_post);
 
 module.exports = router;
