5f83f9340269d046cd2d9af7cbe87c17b3cac884
Nico Nap <nico.nap@outlook.com>
2021-07-28 10:09:41 +0200

    Create a better index page

--- routes/membersonly.js
+++ routes/membersonly.js
@@ -3,9 +3,19 @@ var router = express.Router();
 var signupController = require('../controllers/signupController');
 var joinController = require('../controllers/joinController');
 var loginController = require('../controllers/loginController');
+var async = require('async');
+var Message = require('../models/message');
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome', user: req.user });
+  async.parallel({
+    messages: function(callback) {
+      Message.find().exec(callback);
+    }
+  },
+  function(err, results) {
+    if (err) return next(err);
+    res.render('index', { title: 'Welcome', user: req.user, messages: results.messages });
+  })
 });
 
 router.get('/signup', signupController.signup_get)
